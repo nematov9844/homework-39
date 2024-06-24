636d910f96955dd5d44b8c39ea1a58027ac023bf
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-06-24 23:41:09 +0500

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
@@ -6,13 +7,3 @@ module.exports = {
   plugins: [],
 }
 
-
-// module.exports = {
-//   theme: {
-//     extend: {
-//       gridTemplateColumns: {
-//         "auto-fill-300": "repeat(auto-fill, minmax(300px, 1fr))",
-//       },
-//     },
-//   },
-// };
